21016ede1c3b0898c840ac890598c3d6a737b9a6
MohamedIjlal27 <mohamedijlal27@gmail.com>
2025-01-28 00:01:24 +0530

    Styles Updated

--- app/(screens)/invoice/receipts.tsx
+++ app/(screens)/invoice/receipts.tsx
@@ -42,7 +42,7 @@ export default function InvoiceReceiptsScreen() {
       const today = formatDate(new Date());
       const response = await fetchInvoiceReceipts({ 
         salesperson_id: userId, 
-        date: today
+        date: "2025-01-26"
       });
       
       if (response.result && response.result.orders) {
